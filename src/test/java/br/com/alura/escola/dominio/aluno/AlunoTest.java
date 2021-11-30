f66ada3d425df2d43fd6ce6ccbef5a5d998be817
Gabriel <g.dessilva95@gmail.com>
2021-11-29 21:00:31 -0300

    Java e DDD - Alura

--- src/test/java/br/com/alura/escola/dominio/aluno/AlunoTest.java
+++ src/test/java/br/com/alura/escola/dominio/aluno/AlunoTest.java
@@ -6,6 +6,10 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import br.com.alura.escola.academico.dominio.aluno.Aluno;
+import br.com.alura.escola.academico.dominio.aluno.Email;
+import br.com.alura.escola.shared.dominio.CPF;
+
 class AlunoTest {
 
 	private Aluno aluno;
